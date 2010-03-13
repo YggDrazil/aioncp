327a95553726948a9c6b038c9353ee51d2b594ef
sealnetsoul <sealnetsoul@f53e396e-2a96-11df-8c61-7f7d6f4ad91d>
2010-03-13 17:01:31 +0000

    mini fix tpl

--- trunk/class/core.php
+++ trunk/class/core.php
@@ -1,6 +1,14 @@
 <?php
+/* ------------------------------------------------------------------------
+
+	Free CP for Aoin
+	beta version
+	Developer www.fdcore.ru
+	
+	http://code.google.com/p/aioncp/
+------------------------------------------------------------------------ */
 // DEBUG MODE (bool)
-define('DEBUG', TRUE);
+define('DEBUG', FALSE);
 // Default lang in cp (ru \ end)
 define('DEFAULT_LANG','ru');
 // jquery effect
