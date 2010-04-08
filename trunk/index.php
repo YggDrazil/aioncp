404aa27b2f351b2c6bfa27d08bdcd23ea329c1d2
sealnetsoul <sealnetsoul@f53e396e-2a96-11df-8c61-7f7d6f4ad91d>
2010-04-08 13:23:21 +0000

    RFMP 1.0 Alpha Unstable

--- trunk/index.php
+++ trunk/index.php
@@ -17,6 +17,7 @@ $class_list=array(
 	'controller.php',
 	 DRIVER.'.class.php',
 	'table.class.php',
+	'Quicky.class.php',
 	DRIVER.'.model.php',
 );
 
@@ -56,98 +57,4 @@ if (isset($_GET['action'])) {
 	$ACT=$_GET['action'];
 } else $ACT='';
 	$h='icon-spacer'; // выделенно
-	$unh='icon-no-spacer'; // yе выделенно
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-	"http://www.w3.org/TR/html4/strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:php="http://php.net/xsl">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title><?php echo $cp->title." ".TITLE; ?></title>
-<link rel="stylesheet" href="themes/css/style.css" type="text/css">
-<link type="text/css" href="themes/css/blitzer/jquery-ui-1.8rc3.custom.css" rel="stylesheet" />
-<script type="text/javascript" src="themes/js/jquery.js"></script>
-<script type="text/javascript" src="themes/js/ui.js"></script>  
-</head>
-<body onload="$('#loader').fadeOut()">
- 	<div class='fav'><a href="javascript:void(0)" onclick="$('.ajax_fav').toggle('slow');"><img src='themes/i/bookmark.png' alt='fav'></a></div>
- 	
- 	<div class='ajax_fav hide fav'>
- 		<div class='fav'><a href="javascript:void(0)" onclick="$('.ajax_fav').hide('slow');"><img src='themes/i/dialog_close.png' alt='close'></a></div>
- 		<div class='ajax_fav_load'><?php echo $cp->ajax() ?></div></div>
-	<div id='loader'><img src='themes/i/wait.gif' alt='wait'></div>
-<?php if($cp->logged) { ?>		
-	<div class='topmenu' id='hm'><?php echo $cp->lang['hm'];?></div>
-	<div class='topmenu hide' id='sm'><?php echo $cp->lang['sm'];?></div>
-<?php };?>	
-<div align="center">
-	<table>
-		<tr>
-			<td><table>
-				<tr>
-			<td>
-				<div id='logo'><p><img src="themes/i/logo.png" alt='Logo'><br>AionCP 0.5</p></div>  
-					</td>          
-			<td><div id='hidetopmenu' class='hide'>
-				<div id='hiddenmenu'>
-<?php if($cp->logged) { ?>						
-				<div class="<?php if($ACT=='') echo $unh; else echo $h;?>"> 
-					<p><a href="?"><img src="themes/i/gohome.png" /><br><?php echo $cp->lang['menu_main'];?></a></p> 
-				</div> 
-				<div class="<?php if($ACT=='accounts') echo $unh; else echo $h;?>"> 
-					<p><a href="?action=accounts"><img src="themes/i/kontact_contacts.png" alt=''/><br><?php echo $cp->lang['menu_acclist'];?></a></p> 
-				</div>
-				<div class="<?php if($ACT=='additem')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=additem"><img src="themes/i/list_add.png" alt='' /><br/><?php echo $cp->lang['additemtitle'];?></a></p>
-				</div> 
-				<div class="<?php if($ACT=='charlist')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=charlist"><img src="themes/i/view_media_artist.png" alt='' /><br><?php echo $cp->lang['menu_char_list'];?></a></p>
-				</div> 
-				<div class="<?php if($ACT=='search')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=search"><img src="themes/i/edit_find.png" alt='' /><br><?php echo $cp->lang['menu_search'];?></a></p> 
-				</div>  
-				<div class="<?php if($ACT=='statistic')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=statistic"><img src="themes/i/help_about.png" alt='' /><br><?php echo $cp->lang['menu_stat'];?></a></p> 
-				</div>
-				<!--<div class="<?php if($ACT=='server')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=server"><img src="themes/i/work_server.png" alt='' /><br>Work for server</a></p> 
-				</div> -->
-				
-				<div class="<?php if($ACT=='items')echo $unh; else echo $h;?>"> 
-					<p><a href="?action=items"><img src="themes/i/items.png" alt='' /><br><?php echo $cp->lang['workitem'];?></a></p> 
-				</div> 				
-				
-				<div class="<?php echo $h;?>"> 
-					<p><a href="./backup"><img src="themes/i/backup.png" alt='' /><br>BackUP</a></p> 
-				</div>								  
-				<div class="icon-spacer"> 
-					<p><a href="?action=logout"><img src="themes/i/system_log_out.png" alt='' /><br><?php echo $cp->lang['menu_exit'];?></a></p> 
-				</div> 
-<?php };?>				
-			</div><!-- #hiddenmenu -->				
-			</div></td>
-		</tr>				
-	</table></td>
-		</tr>
-
-<tr>
-	<td><div style='height:20px;'></div><?php if($cp->speedbar!==''){?><div id='speedbar'><?php echo $cp->speedbar;?></div><?php }?></td>
-</tr>
-
-<tr>
-	<td align="left"><form method='post'><?php echo $data?></form></td>
-</tr>	
-</table>
-</div>
-<script type="text/javascript"><?php echo $cp->js?></script>
-<script type="text/javascript" src="themes/js/admincp.js"></script>
-
-
-<div style="font-size:10px; position:fixed; bottom:0px; right:0px;"><a href="http://lab.fdcore.ru" target="_blank" title='Touch me ;)'><img src='themes/i/F.png' alt='FDCore Studio'></a>
-		<a href="http://fdcore.ru/donate" targer="_blank" title="Сделать Добравольное пожертвование"><img src="themes/i/donate.png"></a>
-		<a href='?lang=ru'>RU</a> | 
-		<a href='?lang=en'>EN</a> |
-		<a href='javascript:void(0)' onclick="$('.foo').slideToggle('slow')">Powered by</a> <div class='hide foo'>HTML, <a href='http://php.net' target='_blank'>PHP</a>, XML, CSS 3, JS, <a href='http://mysql.org' target='_blank'>SQL</a>, 
-		<a href='http://jquery.com' target='_blank'>JQuery</a>, 
-		<a href="http://www.oxygen-icons.org/" target='_blank'>Oxygen</a></div></div>
-<!-- Copyright FDcore Studio | Powered by FDcore Labs -->
-</body></html>
+	$unh='icon-no-spacer'; // не выделенно
