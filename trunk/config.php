5abfc96727eda5a7a1bb624e10359565a82bd999
sealnetsoul <sealnetsoul@f53e396e-2a96-11df-8c61-7f7d6f4ad91d>
2010-09-07 00:17:35 +0000

    clear

--- trunk/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* ------------------------------------------------------------------------
-	Configuration file
-*/
-$db_host='localhost'; // Mysql Host
-$db_login='root'; // Mysql username
-$db_password='root'; // Mysql password
-$db_login_server='a_ls'; // login server
-$db_game_server='a_gs'; // game server
--- trunk/themes/main.tpl
+++
@@ -1,4 +0,0 @@
-{include file="header.tpl"}
-{$text}
-
-{include file="footer.tpl"}
